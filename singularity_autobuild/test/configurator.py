cadcf137610acca284ad9acb5a4aea96c36afe54
Hansen <hansen@lip-lin-002541.mpib-berlin.mpg.de>
2018-06-18 14:05:19 +0200

    [refactor] removed leftover comment

--- singularity_autobuild/test/configurator.py
+++ singularity_autobuild/test/configurator.py
@@ -3,7 +3,6 @@
 import os
 import configparser
 
-# TODO change all occurences of this kind
 TEST_BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
 def configure_test_recipe(conf_file: str = TEST_BASE_PATH+'/test.cfg') -> configparser.ConfigParser:
